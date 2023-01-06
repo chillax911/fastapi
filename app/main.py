63127e4abc4f3932d7b8cefe991c990c5c4f87c2
chillax911 <kaijanng@gmail.com>
2023-01-06 09:06:56 +0000

    Minor text differences on two files, to test github portal diff

--- app/main.py
+++ app/main.py
@@ -30,4 +30,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root(): 
-    return {"message": "Hello World! Welcome to the New Year!"}
+    return {"message": "Hello World! Welcome to the New Year!!"}
