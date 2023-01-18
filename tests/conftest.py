6b488fa8156b81184c6f50cf3045e4b3ae8f94d8
chillax911 <kaijanng@gmail.com>
2023-01-18 08:59:15 +0000

    156. Conftest.py 16:18:59

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,3 +33,13 @@ def client(session):
             session.close()                              # Previously, this was 'db.close'
     app.dependency_overrides[get_db] = override_get_db   # This will swap / substitute get_db, this is used extensively through many files. 
     yield TestClient(app)                                # yield is the same as return.
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "hello123@gmail.com", 
+              "password": "password321"}
+    res = client.post("/users", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,7 @@
 import pytest
 from app import schemas
-from .database import client, session
 from jose import jwt
 from app.config import settings
-
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "hello123@gmail.com", 
-              "password": "password321"}
-    res = client.post("/users", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
 
 def test_root(client, session):
     res = client.get("/")
