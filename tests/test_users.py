8595ab6286b2f9960117b88476e9d0ecb48a6c44
chillax911 <kaijanng@gmail.com>
2023-01-18 08:28:09 +0000

    155. Test/validate token 16:14:40

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
 import pytest
 from app import schemas
 from .database import client, session
+from jose import jwt
+from app.config import settings
 
 @pytest.fixture
 def test_user(client):
@@ -8,9 +10,8 @@ def test_user(client):
               "password": "password321"}
     res = client.post("/users", json=user_data)
     assert res.status_code == 201
-    print(res.json())
     new_user = res.json()
-    new_user["password"] = user_data["password"]
+    new_user['password'] = user_data['password']
     return new_user
 
 def test_root(client, session):
@@ -25,6 +26,11 @@ def test_create_user(client):
     assert res.status_code == 201
 
 def test_login_user(client, test_user):
-    res = client.post("/login", data={"username": test_user["email"],
-                                      "password": test_user["password"]  })
+    res = client.post("/login", data={"username": test_user['email'],
+                                      "password": test_user['password']  })
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=settings.algorithm)
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
