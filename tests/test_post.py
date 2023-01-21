f6b2a3753afa1605bfc7dc8b1675add75df16834
chillax911 <kaijanng@gmail.com>
2023-01-21 17:52:38 +0000

    164. Update post 17:15:17 correction 17:15:05

--- tests/test_post.py
+++ tests/test_post.py
@@ -76,20 +76,54 @@ def test_delete_post_success(authorized_client, test_user, test_posts, session):
 
     res = authorized_client.delete(
         f"/posts/{test_posts[0].id}")
-    print("testing")
 
     qty_after = session.query(models.Post).count()   # print(f"Count AFTER: {qty_after}")
     qty_diff = qty_before - qty_after
     assert qty_diff == 1
     assert res.status_code == 204
 
-def test_delete_post_non_exist(authorized_client, test_user, test_posts, session):
+def test_delete_post_non_exist(authorized_client, test_user, test_posts):
     res = authorized_client.delete(
         f"/posts/999999")
-    print("testing")
     assert res.status_code == 404
 
-def test_delete_other_usesr_post(authorized_client, test_user, test_posts, session, test_user2):
+def test_delete_other_usesr_post(authorized_client, test_user, test_posts, test_user2):
     res = authorized_client.delete(
         f"/posts/{test_posts[3].id}")
     assert res.status_code == 403
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data["title"]
+    assert updated_post.content == data["content"]
+
+def test_update_other_user_post(authorized_client, test_user, test_posts, test_user2):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id    # This (zero-base) index position (3) corresponds to the 4th one, created by test_user2, created in conftest.py
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
+
+def test_unauthorised_user_update_posts(client, test_posts):
+    res = client.put(
+        f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id    # This (zero-base) index position (3) corresponds to the 4th one, created by test_user2, created in conftest.py
+    }
+    res = authorized_client.put(
+        f"/posts/999999", json=data)
+    assert res.status_code == 404
